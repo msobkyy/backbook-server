f493969bd0ec529696fe5f055a1b02183b6ac755
Msobkyy <m_e0220@yahoo.com>
2023-01-06 12:40:04 +0200

    fix cross site origin

--- controllers/authController.js
+++ controllers/authController.js
@@ -20,9 +20,8 @@ const createSendToken = (user, statusCode, res) => {
       Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
     ),
     secure: process.env.NODE_ENV === 'production',
-    // secure: true,
-    httpOnly: true,
-    // sameSite: 'None',
+    httpOnly: false,
+    samesite: 'None',
   });
 
   user.password = undefined;
